e81a5a74863ba7fe9ace14c72791dd0359d0c5b7
fonsiOP <152197044+fonsiOP@users.noreply.github.com>
2025-09-24 17:04:36 +0200

    if

--- Repaso/src/Main.java
+++ Repaso/src/Main.java
@@ -5,6 +5,19 @@ public class Main {
 
 
 		System.out.println("Hola a todos");
+		int num = 0;
+
+		// Condicionales
+		if(num>0) {
+			//instruccion 1
+		}else if(num==0){
+			//instruccion 2
+		}else {
+			//instruccion 3
+		}
+		
+		//instruccion 4,5,6,
+		
 
 	}
 
